e14a13106f17be63664b0301ec6cd41237b132f7
zhanghuidong <congmingdexiaoshuaige@gmail.com>
2018-07-19 02:33:19 +0800

    basic

---
+++ GanDengYan/Game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GanDengYan
+{
+    struct Result
+    {
+        public int winner;
+        public int numCards;
+        public int numBomb;
+    }
+
+    class Game
+    {
+        private const int CARD_BIT_COUNT = 5;
+        public uint DoubleCardCount { get; set; }
+        public List<Result> Results { get; private set; }
+        public List<Player> Players { get; private set; }
+
+        public void AddRecord(int numBomb, List<int> numCards)
+        {
+            if (numCards.Count != Players.Count)
+                return;
+
+            int numCardsBitFormat = 0;
+            int winnerScore = 0;
+            int winner = 0;
+            for (int i = 0; i < Players.Count; ++i)
+            {
+                int score = ComputeScore(numBomb, numCards[i]);
+
+                if (score == 0)
+                {
+                    winner = i;
+                    continue;
+                }
+
+                winnerScore -= score;
+                numCardsBitFormat = (numCardsBitFormat << CARD_BIT_COUNT) | numCards[i];
+                Players[i].ScoreRecord.Add(score);
+            }
+            Players[winner].ScoreRecord.Add(winnerScore);
+
+            Result result = new Result();
+            result.winner = winner;
+            result.numCards = numCardsBitFormat;
+            result.numBomb = numBomb;
+            Results.Add(result);
+        }
+
+        private int ComputeScore(int numBomb, int numCard)
+        {
+            if (numCard >= DoubleCardCount)
+                return -numCard * 2 * (numBomb + 1);
+            return -numCard * (numBomb + 1);
+        }
+    }
+}
---
+++ GanDengYan/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GanDengYan
+{
+    class Player
+    {
+        public uint ID { get; set; }
+        public string Name { get; set; } 
+        public int Score { get; set; }
+        public bool Initiative { get; set; }
+        public List<int> ScoreRecord { get; set; }
+    }
+}
